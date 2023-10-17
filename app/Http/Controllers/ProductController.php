f639ac74e692f9a06c964ba594cf036c05a30dc4
ralVovax <141141823+ralVovax@users.noreply.github.com>
2023-10-17 12:27:04 +0800

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json($product, 201);
+    }
+
+    public function show(Product $product)
+    {
+        return response()->json($product, 200);
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        $product->update($validated);
+        return response()->json($product, 200);
+    }
+
+    public function destroy(Product $product) 
+    {
+        $product->delete();
+        return response()->json('Deleted', 200);
+    }
+}
